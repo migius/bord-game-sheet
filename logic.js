0ebfbb85c2325b0f2c012fbf09c7f173831a2275
Bruni Michele <michelebruni87@gmail.com>
2026-02-05 21:02:54 +0100

    rollback

--- logic.js
+++ logic.js
@@ -38,18 +38,16 @@ function loadGioco(id) {
 }
 
 
-function getImage(datiGioco)
-{
+function getImage(datiGioco) {
     stringa = "<img src='" + datiGioco["thumbnail"] + "' />";
 
     stringa += "<img src='" + datiGioco["image"] + "' />";
- 
+
     return stringa;
 }
- 
 
-function GiocoSelezionato()
-{        
+
+function GiocoSelezionato() {
     var selectGioco = document.getElementById('selectGioco');
     main.id = selectGioco.value;
     loadGioco(selectGioco.value);
@@ -79,16 +77,15 @@ var main = new Vue({
         image: ""
     },
     computed: {
-        infoInRiga: function(){                    
+        infoInRiga: function () {
             //es. Autori: Isaac Childres | Artisti: Alexandr Elichev, Josh T. McDowell, Alvaro Nebot | Giocatori: 1-4 | Durata: 60-120′ | Età: 14+ | Editore: Asmodee Italia
-         
+
             let components = [];
 
-            if(this.designer.length > 0)
-            {
+            if (this.designer.length > 0) {
                 let stringa = "";
 
-                if(this.designer.length > 1)
+                if (this.designer.length > 1)
                     stringa += "Autori: ";
                 else
                     stringa += "Autore: ";
@@ -98,17 +95,16 @@ var main = new Vue({
                 components.push(stringa)
             }
 
-            if(this.artist.length > 0)
-            {
+            if (this.artist.length > 0) {
                 let stringa = "";
 
-                if(this.artist.length > 1)
+                if (this.artist.length > 1)
                     stringa += "Artisti: ";
                 else
                     stringa += "Artista: ";
 
-                stringa += this.artistJoin;      
-                components.push(stringa)          
+                stringa += this.artistJoin;
+                components.push(stringa)
             }
 
             components.push("Giocatori: " + this.players);
@@ -117,120 +113,98 @@ var main = new Vue({
 
             components.push("Età: " + this.minage + "+");
 
-            if(this.publisher.length > 0)
-            {
+            if (this.publisher.length > 0) {
                 let stringa = "";
 
-                if(this.publisher.length > 1)
+                if (this.publisher.length > 1)
                     stringa += "Editori: ";
                 else
                     stringa += "Editore: ";
 
-                stringa += this.publisherJoin;      
-                components.push(stringa)          
+                stringa += this.publisherJoin;
+                components.push(stringa)
             }
 
-            return components.join(" | ");      
+            return components.join(" | ");
         },
-        players: function(){
+        players: function () {
             return this.minplayers + (this.minplayers !== this.maxplayers ? "-" + this.maxplayers : "");
         },
-        playtime: function(){
+        playtime: function () {
             return this.minplaytime + (this.minplaytime !== this.maxplaytime ? "-" + this.maxplaytime : "")
         },
-        sheetImage: function(){
-            return "https://geekpizza.altervista.org/@PizzaMeepleBot/Images/schedagioco/?" + 
-                "t=" + encodeURIComponent(this.gameName) + 
-                "&a=" + encodeURIComponent(this.designerJoin) + 
-                "&i=" + encodeURIComponent(this.artistJoin) + 
-                "&e=" + encodeURIComponent(this.publisherJoin) + 
-                "&m=" + this.players + 
+        sheetImage: function () {
+            return "https://geekpizza.altervista.org/@PizzaMeepleBot/Images/schedagioco/?" +
+                "t=" + encodeURIComponent(this.gameName) +
+                "&a=" + encodeURIComponent(this.designerJoin) +
+                "&i=" + encodeURIComponent(this.artistJoin) +
+                "&e=" + encodeURIComponent(this.publisherJoin) +
+                "&m=" + this.players +
                 "&n=" + this.playtime +
-                "&l=" + this.minage + 
-                "&p=" + this.price + 
-                "&j=" + this.thumbnail; 
+                "&l=" + this.minage +
+                "&p=" + this.price +
+                "&j=" + this.thumbnail;
         }
     },
-    methods: { 
-        loadDummy: function(){
+    methods: {
+        loadDummy: function () {
             //todo
 
         },
-        updateGioco: function(gameData)
-        {
+        updateGioco: function (gameData) {
             try {
-                // Convert XML to JSON with better error handling
-                json_datiGioco = xml2json(gameData);
-                
-                // Remove "undefined" strings at the beginning
-                json_datiGioco = json_datiGioco.replace(/^undefined/g, '');
-                
-                // Remove undefined in the middle of the JSON
-                json_datiGioco = json_datiGioco.replace(/"undefined"/g, '""');
-                
-                // Also handle potential trailing commas that break JSON
-                json_datiGioco = json_datiGioco.replace(/,(\s*[}\]])/g, '$1');
-                
-                console.log("JSON string:", json_datiGioco);
-                
+                json_datiGioco = xml2json(gameData).replace("undefined", "");
                 datiGioco = JSON.parse(json_datiGioco).items.item;
-                
-                // Handle game name - check if it's an array or object
-                if (Array.isArray(datiGioco.name)) {
-                    // If it's an array, find the primary name
-                    var primaryName = datiGioco.name.find(function(n){ return n["@type"] == "primary"; });
-                    this.gameName = primaryName ? primaryName["@value"] : datiGioco.name[0]["@value"];
-                } else if (datiGioco.name && datiGioco.name["@value"]) {
-                    // If it's a single object with @value
-                    this.gameName = datiGioco.name["@value"];
-                } else {
-                    this.gameName = "Nome non disponibile";
-                }
 
-                // Handle links - ensure it's always an array
-                var links = Array.isArray(datiGioco.link) ? datiGioco.link : [datiGioco.link];
-                
-                this.designer = links.filter(function(link){return link && link["@type"] === "boardgamedesigner";}).map(function(c){return c["@value"];});            
+                if (datiGioco.name["@value"] !== undefined)
+                    this.gameName = datiGioco.name["@value"];
+                else
+                    this.gameName = datiGioco.name.filter(function (n) { return n["@type"] == "primary"; })[0]["@value"];
+
+                this.designer = datiGioco.link.filter(function (link) { return link["@type"] === "boardgamedesigner"; }).map(function (c) { return c["@value"]; });
                 this.designerJoin = this.designer.join(", ");
-                
-                this.artist = links.filter(function(link){return link && link["@type"] === "boardgameartist";}).map(function(c){return c["@value"];});
+                this.artist = datiGioco.link.filter(function (link) { return link["@type"] === "boardgameartist"; }).map(function (c) { return c["@value"]; });
                 this.artistJoin = this.artist.join(", ");
-                
-                this.publisher = links.filter(function(link){return link && link["@type"] === "boardgamepublisher";}).map(function(c){return c["@value"];});
+                this.publisher = datiGioco.link.filter(function (link) { return link["@type"] === "boardgamepublisher"; }).map(function (c) { return c["@value"]; });
                 this.publisherJoin = this.publisher.join(", ");
 
-                this.minplayers = datiGioco.minplayers ? datiGioco.minplayers["@value"] : 0;
-                this.maxplayers = datiGioco.maxplayers ? datiGioco.maxplayers["@value"] : 0;
+                this.minplayers = datiGioco.minplayers["@value"];
+                this.maxplayers = datiGioco.maxplayers["@value"];
 
-                this.minplaytime = datiGioco.minplaytime ? datiGioco.minplaytime["@value"] : 0;
-                this.maxplaytime = datiGioco.maxplaytime ? datiGioco.maxplaytime["@value"] : 0;
+                this.minplaytime = datiGioco.minplaytime["@value"];
+                this.maxplaytime = datiGioco.maxplaytime["@value"];
 
-                this.minage = datiGioco.minage ? datiGioco.minage["@value"] : 0;
+                this.minage = datiGioco.minage["@value"];
 
-                this.thumbnail = datiGioco["thumbnail"] || "";
-                this.thumbnail_static = datiGioco["thumbnail"] || "";
-                this.image = datiGioco["image"] || "";
+
+                this.thumbnail = datiGioco["thumbnail"];
+                this.thumbnail_static = datiGioco["thumbnail"];
+
+                this.image = datiGioco["image"];
+
+                //document.getElementsByClassName("info-text")[0].innerText = infoInRiga(datiGioco);            
+                //document.getElementsByClassName("game-url")[0].innerHTML = getURL(datiGioco, gameName);
+                //document.getElementsByClassName("game-img")[0].innerHTML = getImage(datiGioco);
 
                 ValorizzaCode();
-                
+
             } catch (error) {
                 console.error("Errore nel parsing dei dati del gioco:", error);
                 console.error("JSON problematico:", json_datiGioco);
-                alert("Errore nel caricamento dei dati del gioco. Controlla la console per dettagli.");
             }
-        }   
+        }
     },
-    beforeCreate: function(){
+    beforeCreate: function () {
         console.log("beforeCreate");
     },
-    created: function(){
+    created: function () {
         console.log("created");
         console.log("end created");
     },
-    mounted: function(){
+    mounted: function () {
         console.log("mounted");
         //after all loaded
-        this.$nextTick(function() {
+        this.$nextTick(function () {
             console.log("nextTick");
             console.log("end nextTick");
         });
@@ -238,7 +212,8 @@ var main = new Vue({
 });
 
 Vue.directive('visible', (el, bind) => {
-    el.style.visibility=(!!bind.value) ? 'visible' : 'hidden';});
+    el.style.visibility = (!!bind.value) ? 'visible' : 'hidden';
+});
 
 
 
@@ -306,9 +281,9 @@ xhr.onload = function () {
 
 function loadGiochi() {
 
-    var stringaGioco =  document.getElementById("giocoColl").value;
-    
-    if(stringaGioco.length < 3) return;
+    var stringaGioco = document.getElementById("giocoColl").value;
+
+    if (stringaGioco.length < 3) return;
 
     url = "https://geekpizza.altervista.org/@PizzaMeepleBot/Functions/search.php?query=";
     //difficolta
@@ -318,21 +293,20 @@ function loadGiochi() {
     xhr.send();
 }
 
-function updateGiochi(data)
-{
-    var giochi =  Array.from(data.documentElement.children);
-    
+function updateGiochi(data) {
+    var giochi = Array.from(data.documentElement.children);
+
     var selectGioco = document.getElementById('selectGioco');
-    
+
     //svuoto
     var length = selectGioco.options.length;
-    for (i = length-1; i >= 0; i--) {
-      selectGioco.options[i] = null;
+    for (i = length - 1; i >= 0; i--) {
+        selectGioco.options[i] = null;
     }
-    
+
     //aggiungo
-    for(var i = 0, l = giochi.length; i < l; i++){
-      selectGioco.options.add( new Option(giochi[i].children[0]?.attributes["value"]?.value + " (" + giochi[i].children[1]?.attributes["value"]?.value + ")" , giochi[i].id, false) );
+    for (var i = 0, l = giochi.length; i < l; i++) {
+        selectGioco.options.add(new Option(giochi[i].children[0]?.attributes["value"]?.value + " (" + giochi[i].children[1]?.attributes["value"]?.value + ")", giochi[i].id, false));
     }
     GiocoSelezionato();
 }
@@ -352,12 +326,12 @@ function updateGiochi(data)
 
 //UTILITY EXPORT
 
-function nodeToString ( node ) {
-   var tmpNode = document.createElement( "div" );
-   tmpNode.appendChild( node.cloneNode( true ) );
-   var str = tmpNode.innerHTML;
-   tmpNode = node = null; // prevent memory leaks in IE
-   return str;
+function nodeToString(node) {
+    var tmpNode = document.createElement("div");
+    tmpNode.appendChild(node.cloneNode(true));
+    var str = tmpNode.innerHTML;
+    tmpNode = node = null; // prevent memory leaks in IE
+    return str;
 }
 
 function ValorizzaCode() {
